2b1ed94afdc2c00b36e13a60f9afe41a907929e9
anaijg <ana.jimenezgomez@educa.madrid.org>
2026-09-18 18:13:48 +0200

    Ejercicios 01 1 y 2

---
+++ Apuntes.md
@@ -0,0 +1,13 @@
+# Comentarios
+En Python, los comentarios se escriben con el símbolo #.
+O ''' según Víctor.
+# Mostrar por pantalla
+print()
+# División
+/ es división con decimales
+// es división entera
+
+# Pedir un dato por teclado
+variable = input("Dime tu nombre:")
+# Si queremos forzar el tipo de variable, hacemos un casting
+variable = int(input("Número 1: "))
---
+++ Ejercicios01.py
@@ -0,0 +1,25 @@
+# Ejercicios de Python
+""" Comentario"""
+print("Hola Mundo")
+
+## Operaciones con tipos enteros
+### Ejercicio 1
+num1 = 12
+num2 = 4
+print("Suma:", (num1 + num2))
+print("Resta:", (num1 - num2))
+print("Multiplicación:", (num1 * num2))
+print("División:", (num1 // num2))
+print("Módulo:", (num1 % num2))
+
+## Ejercicio 2 
+numero1 = int(input("Número 1: "))
+numero2 = int(input("Número 2: "))
+numero3 = int(input("Número 3: "))
+numero4 = int(input("Número 4: "))
+
+print("Suma:", (numero1 + numero2 + numero3 + numero4))
+print("Resta:", (numero1 - numero2 - numero3 - numero4))
+print("Multiplicación:", (numero1 * numero2 * numero3 * numero4))
+print("División:", (numero1 // numero2 // numero3 // numero4))
+print("Módulo:", (numero1 % numero2 % numero3 % numero4))
